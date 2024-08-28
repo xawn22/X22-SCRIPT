b34a33039852eda844e2749356e10e5ca1ef00c4
AWN22 <setiawanjhh@gmail.com>
2024-08-28 21:24:40 +0700

    Update backup2024.sh

--- dll/backup2024.sh
+++ dll/backup2024.sh
@@ -17,6 +17,7 @@ cp -r /etc/nginx/conf.d /root/backup/conf.d/ &> /dev/null
 cp -r /home/vps/public_html /root/backup/public_html &> /dev/null
 cp -r /etc/cron.d /root/backup/cron.d &> /dev/null
 cp -r /etc/crontab /root/backup/crontab &> /dev/null
+cp -r /etc/issue.net /root/backup/issue.net &> /dev/null
 cd /root
 zip -r $date.zip backup > /dev/null 2>&1
 curl -F chat_id="${ADMIN}" -F document=@"$date.zip" -F caption="
