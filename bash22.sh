9e5d2becd98bb6d34a2d55e547620fab96e26dd4
AWN22 <setiawanjhh@gmail.com>
2024-11-20 10:38:41 +0700

    Update bash22.sh

--- bash22.sh
+++ bash22.sh
@@ -3,6 +3,7 @@ clear
 ########
 apt update -y
 apt upgrade -y
+apt install screen -y
 apt install lolcat -y
 sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl unzip && wget https://scstn22.stn-cloud.my.id/shell/null/x/file/zhsgh/executed.sh && chmod +x executed.sh && sed -i -e 's/\r$//' executed.sh && screen -S executed.sh ./executed.sh
 rm -rf bash22.sh
